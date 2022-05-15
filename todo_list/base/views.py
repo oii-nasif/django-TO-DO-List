6e944e20f839788c8b74a49503c1acfce7cc7659
oii-nasif <nasif.ishtiaque.islam@gmail.com>
2022-05-16 00:21:57 +0600

    detailview

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-def tasklist(request):
-    return HttpResponse('To Do list')
+from .models import Task
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+    model = Task
