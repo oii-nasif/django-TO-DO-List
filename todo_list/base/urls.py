995584176c3c9007a03547f1272ed4e0a3347128
oii-nasif <nasif.ishtiaque.islam@gmail.com>
2022-05-20 12:49:44 +0600

    CRUD implementation done!

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import TaskList
+from .views import TaskDetail, TaskList, TaskCreate, TaskUpdate, DeleteView
 
 urlpatterns =[
     path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('create-task/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),
 ]
