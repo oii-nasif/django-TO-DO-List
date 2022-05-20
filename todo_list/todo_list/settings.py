6c2937f369b96a4d70568d8d3bb832b248929e65
oii-nasif <nasif.ishtiaque.islam@gmail.com>
2022-05-20 16:05:09 +0600

    registration and search updated

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -106,12 +106,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Dhaka'
 
 USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_URL = 'login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
