8f9ac97b29fa9723eee378565cb09fdc406888ff
oii-nasif <nasif.ishtiaque.islam@gmail.com>
2022-05-20 14:42:11 +0600

    login/logout added

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -4,7 +4,18 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
+from django.contrib.auth.views import LoginView
+
 from .models import Task
+
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = "__all__"
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 
 class TaskList(ListView):
     model = Task
@@ -32,3 +43,4 @@ class DeleteView(DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
+    
